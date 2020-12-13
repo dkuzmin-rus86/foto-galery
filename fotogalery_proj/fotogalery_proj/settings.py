fe5c8e341e9047752f7c8c93dc0794b1abcb5e55
Дмитрий Кузьмин <dkuzmin@MacBook-Pro-Dmitrij.local>
2020-12-13 22:03:46 +0500

    change settings

--- fotogalery_proj/fotogalery_proj/settings.py
+++ fotogalery_proj/fotogalery_proj/settings.py
@@ -121,8 +121,6 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# STATIC_ROOT = '/home/user/hantaton/static/'
-
 LOGIN_REDIRECT_URL =  '/'
 
 LANGUAGE_CODE = 'ru-ru'
